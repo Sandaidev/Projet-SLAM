13b6de37246997f6e3c9fdbb057287116ffed7e2
Sandai <42770217+Sandaidev@users.noreply.github.com>
2020-12-08 14:26:33 +0100

    Refactor : Gerer_monCompte.php

--- Gerer_monCompte.php
+++ Gerer_monCompte.php
@@ -2,10 +2,11 @@
 include("autoload.php");
 
 
-if(isset($_SESSION["idUtilisateur"])) {
+if (isset($_SESSION["idUtilisateur"])) {
     // Si l'utilisateur est connecté
 
     $connexion = Creer_Connexion();
+
     if(isset($_REQUEST["changerMDP"]))
     {
         // Il a cliqué sur changer Mot de passe.
@@ -13,6 +14,7 @@ if(isset($_SESSION["idUtilisateur"])) {
         Vue_Administration_Menu();
         Vue_ModifierMDP_SuperAdmin();
     }
+
     elseif(isset($_REQUEST["SeDeconnecter"]))
     {
         // L'utilisateur a cliqué sur "se déconnecter"
@@ -21,30 +23,36 @@ if(isset($_SESSION["idUtilisateur"])) {
         Vue_Structure_Entete();
         Vue_Connexion_Formulaire_connexion_administration();
     }
-    elseif (isset($_REQUEST["changerMDP_confirmation"])) {
+
+    elseif (isset($_REQUEST["changerMDP_confirmation"]))
+    {
+        // L'utilisateur a confirmé le changement de son mot de passe
+
         Vue_Structure_Entete();
         Vue_Administration_Menu();
 
+        // Récupération des informations de l'utilisateur actuel avant modification
         $utilisateur_selectionne = Utilisateur_Select_ParId($connexion, $_SESSION["idUtilisateur"]);
 
-        // On check si l'ancien mot de passe est OK avec celui sur la BDD...
-        if (!password_verify($_REQUEST["old_pass"], $utilisateur_selectionne["motDePasse"])) {
-            // Si c'est NON
-            Vue_ModifierMDP_SuperAdmin("Ancien mot de passe incorrect!");
-        } else {
-            // Sinon, on check si les nouveaux mot de passe sont les mêmes
-            if ($_REQUEST["new_pass"] != $_REQUEST["new_pass_confirm"]) {
-                // Si c'est pas les même, on arrête là et on affiche un message d'erreur
-                Vue_ModifierMDP_SuperAdmin("Les nouveaux mot de passe ne coïncident pas");
-            } else {
-                // Le mot de passe est OK, on le modifie dans la BDD!
-                Utilisateur_Modifier_motDePasse($connexion, $_SESSION["idUtilisateur"], $_REQUEST["new_pass"]);
-                echo "<p>Votre mot de passe a été modifié!</p>";
-            }
+        // Vérifications : L'ancien mot de passe doit être correct et les nouveaux doivent êtres corrects
+        if (password_verify($_REQUEST["old_pass"], $utilisateur_selectionne["motDePasse"])
+            and $_REQUEST["new_pass"] == $_REQUEST["new_pass_confirm"])
+        {
+            // Le mot de passe est OK, on le modifie dans la BDD!
+            Utilisateur_Modifier_motDePasse($connexion, $_SESSION["idUtilisateur"], $_REQUEST["new_pass"]);
+            echo "<p>Votre mot de passe a été modifié!</p>";
+        }
+
+        else
+        {
+            // L'utilisateur a rentré un mauvais mot de passe (ancien ou nouveau)
+            // On affiche un message d'erreur
+            Vue_ModifierMDP_SuperAdmin("L'ancien mot de passe est invalide ou les nouveaux mot de passe ne coïncident pas.");
         }
     }
 
-    else {
+    else
+    {
         // Cas par défaut: affichage du menu des actions.
         Vue_Structure_Entete();
         Vue_Administration_Menu();
@@ -55,10 +63,12 @@ if(isset($_SESSION["idUtilisateur"])) {
         Vue_Administration_Gerer_Compte($nom_utilisateur);
     }
 }
+
 else
 {
     // On renvoie l'utilisateur à la page de connexion. Il n'aurait jamais du arriver ici !
     Vue_Structure_Entete();
     Vue_Connexion_Formulaire_connexion_administration();
 }
+
 Vue_Structure_BasDePage();
