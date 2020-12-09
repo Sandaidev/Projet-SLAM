25436fb743b10d8c642814575b76efcf34fbf0c2
Sandai <42770217+Sandaidev@users.noreply.github.com>
2020-12-09 15:50:07 +0100

    Refactor connexionAdmin.php

--- connexionAdmin.php
+++ connexionAdmin.php
@@ -4,35 +4,69 @@ include_once("autoload.php");
 /**
  * Contrôleur gérant la connexion à la page administration.
  * En cas de succès, il affiche uniquement le menu d'administration.
+ * -----------------------------------------------------------------
+ * La connexion se fait UNIQUEMENT si l'utilisateur est activé dans la BDD
  */
 
 
+if (    isset($_REQUEST["login"])
+     && isset($_REQUEST["password"])
+) {
+    // Si tous les paramètres du formulaire sont OK
+    // Alors on vérifie le mot de passe
 
-if (isset($_REQUEST["login"]) and isset($_REQUEST["password"])) {//Si tous les paramètres du formulaire sont bons
     $connexion = Creer_Connexion();
-    //Vérification du mot de passe
     $utilisateur = Utilisateur_Select_ParLogin($connexion, $_REQUEST["login"]);
-    if ($utilisateur != null) {
-        if (password_verify($_REQUEST["password"], $utilisateur["motDePasse"])) {//le mot de passe est associable à ce Hash
 
-            $_SESSION["idUtilisateur"] = $utilisateur["idUtilisateur"];
+    if ($utilisateur != null)
+    {
+        if (password_verify($_REQUEST["password"], $utilisateur["motDePasse"]))
+        {
+            // Cas : Mot de passe OK
             Vue_Structure_Entete();
-            Vue_Administration_Menu();
-        } else {//mot de passe pas bon
+
+            if ($utilisateur["statusUtilisateur"] == "1")
+            {
+                $_SESSION["idUtilisateur"] = $utilisateur["idUtilisateur"];
+                Vue_Administration_Menu();
+            }
+
+            else
+            {
+                $msgError = "Votre compte a été désactivé par un administrateur.";
+            }
+        }
+
+        else
+        {
+            // Cas : Mot de passe erroné
             $msgError = "Mot de passe erroné";
             Vue_Structure_Entete();
             Vue_Connexion_Formulaire_connexion_administration($msgError);
         }
-    } else {
+    }
+
+    else
+    {
         $msgError = "Login non trouvé";
         Vue_Structure_Entete();
         Vue_Connexion_Formulaire_connexion_administration($msgError);
     }
-} else {   //Il y a un raté quelque part !
-    if (isset($_REQUEST["login"]) or isset($_REQUEST["password"]))
-        $msgError = "Vous devez saisir toutes les informations";
-    else
+}
+
+else
+{
+    // Cas : Login/mot de passe pas définis
+    if ($_REQUEST["login"] == "")
+    {
         $msgError = "";
+    }
+
+    else
+    {
+        $msgError = "Vous devez saisir toutes les informations";
+    }
+
     Vue_Structure_Entete();
     Vue_Connexion_Formulaire_connexion_administration($msgError);
 }
