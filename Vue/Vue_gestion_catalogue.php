9304cc6df3c78a95ed3885b48cea02583c35c3c4
Sandai <42770217+Sandaidev@users.noreply.github.com>
2020-12-21 11:38:39 +0100

    Create category management draft

--- Vue/Vue_gestion_catalogue.php
+++ Vue/Vue_gestion_catalogue.php
@@ -1,1 +1,15 @@
 <?php
+
+function Vue_menu_ajouter_categorie($listeCategories)
+{
+	// DRAFT FILE; TO BE FINISHED LATER ON
+	echo "
+    <nav style='height: auto;'>
+        <ul>
+    		<li>
+    			<i class='fas fa-folder-plus'></i>
+			</li>
+        </ul>
+    </nav>";
+
+}
