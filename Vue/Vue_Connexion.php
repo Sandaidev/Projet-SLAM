0734c935387768d71a45236242ec5baaf2541250
Sandaidev <erwan.egasse@gmail.com>
2021-01-05 11:20:23 +0100

    Redesign login screen

--- Vue/Vue_Connexion.php
+++ Vue/Vue_Connexion.php
@@ -5,20 +5,27 @@
 function Vue_Connexion_Formulaire_connexion_entreprise($msgErreur = "")
 {
     echo "
-    <form action='connexion.php' style='    width: 50%;    display: block;    margin: auto;'>
-        <h1>Connexion</h1>
+    <form action='connexion.php' style='width: 40%; display: block; margin: auto; margin-top: 26vh; box-shadow: cyan 0px 0px 15px; border-radius: 20px;'>
+        <h1>Entreprise</h1>
+        <h2 style='width: 50%; text-align: center; margin: auto'>— Connexion —</h2>
+        
+        <hr>    
                 
         <label><b>Compte</b></label>
-        <input type=\"text\" placeholder=\"identifiant du compte\" name=\"compte\" required>
+        <input type=\"text\" placeholder=\"login\" name=\"compte\" required>
 
         <label><b>Mot de passe</b></label>
         <input type=\"password\" placeholder=\"mot de passe\" name=\"password\" required>
 
-        <input type=\"submit\" id='submit' value='Se connecter' >";
+        <input type=\"submit\" id='submit' value='Se connecter'>
+        
+        <hr>
+        
+        <a href='connexionAdmin.php'> < Connexion : Administration </a>";
 
     if($msgErreur != "")
     {
-        echo "<label><b>Erreur : $msgErreur</b></label>";
+        echo "<h3>Erreur : $msgErreur</h3>";
     }
 
     echo "</form>";
@@ -27,22 +34,28 @@ function Vue_Connexion_Formulaire_connexion_entreprise($msgErreur = "")
 function Vue_Connexion_Formulaire_connexion_administration($msgErreur = "")
 {
     echo "
-        <form action='connexionAdmin.php' style='width: 50%; display: block; margin: auto;'>
-            <h1>Administration : Connexion</h1>
-                
-            <label><b>Identifiant de connexion </b></label>
-            <input type=\"text\" placeholder=\"login\" name=\"login\" required>
+        <form action='connexionAdmin.php' style='width: 40%; display: block; margin: auto; margin-top: 26vh; box-shadow: cyan 0px 0px 15px; border-radius: 20px;'>
+        
+        <h1>Administration</h1>
+        <h2 style='width: 50%; text-align: center; margin: auto'>— Connexion —</h2>
+        
+        <hr>     
+        <label><b>Compte</b></label>
+        <input type=\"text\" placeholder=\"login\" name=\"login\" required>
 
-            <label><b>Mot de passe</b></label>
-            <input type=\"password\" placeholder=\"mot de passe\" name=\"password\" required>
+        <label><b>Mot de passe</b></label>
+        <input type=\"password\" placeholder=\"mot de passe\" name=\"password\" required>
 
-            <input type=\"submit\" id='submit' value='Se connecter' >
-            <a href='../connexion.php'> < Connexion : Entreprises </a>
-            ";
+        <input type=\"submit\" id='submit' value='Se connecter' >
+        
+        <hr>
+        
+        <a href='connexion.php'> < Connexion : Entreprises </a>
+        ";
 
     if($msgErreur != "")
     {
-        echo " <label><b>Erreur : $msgErreur</b></label>";
+        echo "<h4>Erreur : $msgErreur</h4>";
     }
 
     echo "</form>";
