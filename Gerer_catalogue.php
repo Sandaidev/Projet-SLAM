34569a3d0bd361be1793bbc0787548407b946938
Sandaidev <erwan.egasse@gmail.com>
2021-01-10 13:28:38 +0100

    Implement file upload

--- Gerer_catalogue.php
+++ Gerer_catalogue.php
@@ -7,37 +7,80 @@ include("autoload.php");
 
 Vue_Structure_Entete();
 
-if(isset($_SESSION["idUtilisateur"]))
-{
-	// Cas : L'utilisateur est connecté
-	// -> On affiche la navbar
-	Vue_Administration_Menu();
+if(isset($_SESSION["idUtilisateur"])) {
+    // Cas : L'utilisateur est connecté
+    // -> On affiche la navbar
+    Vue_Administration_Menu();
 
-	// Connexion à la BDD
-	$connexion = Creer_Connexion();
+    // Connexion à la BDD
+    $connexion = Creer_Connexion();
 
-	// Affichage des catégories dans la navbar + bouton "créer catégorie"
-	$liste_categories = Categorie_select($connexion);
-	Vue_Catalogue_menu($liste_categories, true);
+    // Affichage des catégories dans la navbar + bouton "créer catégorie"
+    $liste_categories = Categorie_select($connexion);
+    Vue_Catalogue_menu($liste_categories, true);
+    print_debug($_FILES);
+    // Si on a un upload d'image
+    if (isset($_FILES['fichier']))
+    {
 
-	if (isset($_REQUEST["confirmation_modifier_produit"]))
-	{
-		// Cas : CONFIRMATION de modification d'un produit
+        print_r("Dans isset fichier");
+        $errors = array();
+        $file_name = $_FILES['fichier']['name'];
+        $file_size = $_FILES['fichier']['size'];
+        $file_tmp = $_FILES['fichier']['tmp_name'];
+        $file_type = $_FILES['fichier']['type'];
+        $tab = explode('.', $_FILES['fichier']['name']);
+        $file_ext = strtolower(end($tab));
+        $expensions = array("jpeg", "jpg", "png", "mp3",
+            "acc", "wav", "3gpp", "mp4", "3gp", "m4a", "amr", "avi",
+            "flv", "gif"); //Peut être des extensions en trop...
 
-		produit_modifier(
-			$connexion,
-			$_REQUEST["id_produit"],
-			$_REQUEST["id_categorie"],
-			$_REQUEST["nom_produit"],
-			$_REQUEST["description"],
-			$_REQUEST["prix_ht"],
-			$_REQUEST["resume"]);
+        if (in_array($file_ext, $expensions) === false)
+        {
+            $errors[] = "extension not allowed, please choose a JPEG or PNG file.";
+        }
 
-		$liste_produits = produit_select($connexion);
-		Vue_afficher_liste_produits($liste_produits);
-	}
+        if ($file_size > 2097152666655)
+        {
+            $errors[] = 'File size must be less than 2 MB';
+        }
 
-	elseif (isset($_REQUEST["confirmation_modifier_categorie"]))
+        if (empty($errors) == true)
+        {
+            move_uploaded_file($file_tmp, '.\\' . $file_name);
+            produit_modifier_url_image($connexion, $_REQUEST["idProduit"], $file_tmp);
+
+            $infos_produit = produit_selectParID($connexion, $_REQUEST["idProduit"]);
+            $liste_categories = Categorie_select($connexion);
+            $liste_tva = TVA_select($connexion);
+            Vue_formulaire_modification_produit($liste_categories, $infos_produit, false, $liste_tva);
+        }
+
+        else
+        {
+            //Erreur quand il y a l'upload
+            print_debug($errors);
+        }
+    }
+
+    if (isset($_REQUEST["confirmation_modifier_produit"]))
+    {
+        // Cas : CONFIRMATION de modification d'un produit
+
+        produit_modifier(
+            $connexion,
+            $_REQUEST["id_produit"],
+            $_REQUEST["id_categorie"],
+            $_REQUEST["nom_produit"],
+            $_REQUEST["description"],
+            $_REQUEST["prix_ht"],
+            $_REQUEST["resume"]);
+
+        $liste_produits = produit_select($connexion);
+        Vue_afficher_liste_produits($liste_produits);
+    }
+
+    elseif (isset($_REQUEST["confirmation_modifier_categorie"]))
     {
         Categorie_Modifier(
             $connexion,
@@ -49,7 +92,7 @@ if(isset($_SESSION["idUtilisateur"]))
         Vue_afficher_liste_gestion_categories($liste_categories);
     }
 
-	elseif (isset($_REQUEST["activer_liste_produits"]))
+    elseif (isset($_REQUEST["activer_liste_produits"]))
     {
         if (isset($_REQUEST["liste_produit"]))
         {
@@ -65,10 +108,10 @@ if(isset($_SESSION["idUtilisateur"]))
         $liste_categories = Categorie_select($connexion);
         $liste_tva = TVA_select($connexion);
         Vue_afficher_liste_gestion_produits($liste_produits, $liste_categories, $liste_tva);
+
     }
 
-	elseif (isset($_REQUEST["desactiver_liste_produits"]))
-    {
+    elseif (isset($_REQUEST["desactiver_liste_produits"])) {
         if (isset($_REQUEST["liste_produit"]))
         {
             $liste_produits = $_REQUEST["liste_produit"];
@@ -85,7 +128,7 @@ if(isset($_SESSION["idUtilisateur"]))
         Vue_afficher_liste_gestion_produits($liste_produits, $liste_categories, $liste_tva);
     }
 
-	elseif (isset($_REQUEST["supprimer_liste_produits"]))
+    elseif (isset($_REQUEST["supprimer_liste_produits"]))
     {
         if (isset($_REQUEST["liste_produit"]))
         {
@@ -103,7 +146,7 @@ if(isset($_SESSION["idUtilisateur"]))
         Vue_afficher_liste_gestion_produits($liste_produits, $liste_categories, $liste_tva);
     }
 
-	elseif (isset($_REQUEST["supprimer_liste_categories"]))
+    elseif (isset($_REQUEST["supprimer_liste_categories"]))
     {
         if (isset($_REQUEST["liste_categories"]))
         {
@@ -158,32 +201,26 @@ if(isset($_SESSION["idUtilisateur"]))
         Vue_afficher_liste_gestion_categories($liste_categories);
     }
 
-
-	elseif (isset($_REQUEST["creer_produit"]))
-	{
-		// Cas : Création d'un produit (affichage du formulaire)
-		$liste_categories = Categorie_select($connexion);
-		$liste_tva = TVA_select($connexion);
-		Vue_formulaire_modification_produit($liste_categories, null, true, $liste_tva);
-	}
-
-	elseif (isset($_REQUEST["produit_upload_image"]))
+    elseif (isset($_REQUEST["creer_produit"]))
     {
-
+        // Cas : Création d'un produit (affichage du formulaire)
+        $liste_categories = Categorie_select($connexion);
+        $liste_tva = TVA_select($connexion);
+        Vue_formulaire_modification_produit($liste_categories, null, true, $liste_tva);
     }
 
-	elseif (isset($_REQUEST["confirmation_creer_produit"]))
-	{
-		// Cas : CONFIRMATION de création d'un produit
+    elseif (isset($_REQUEST["confirmation_creer_produit"]))
+    {
+        // Cas : CONFIRMATION de création d'un produit
         produit_creer(
-          $connexion,
-          $_REQUEST["id_categorie"],
-          $_REQUEST["nom_produit"],
-          $_REQUEST["description"],
-          $_REQUEST["prix_ht"],
-          $_REQUEST["resume"],
-          $_REQUEST["status_produit"],
-          $_REQUEST["idTVA"]
+            $connexion,
+            $_REQUEST["id_categorie"],
+            $_REQUEST["nom_produit"],
+            $_REQUEST["description"],
+            $_REQUEST["prix_ht"],
+            $_REQUEST["resume"],
+            $_REQUEST["status_produit"],
+            $_REQUEST["idTVA"]
         );
 
         $liste_produits = produit_select($connexion);
@@ -191,23 +228,23 @@ if(isset($_SESSION["idUtilisateur"]))
         $liste_tva = TVA_select($connexion);
         Vue_afficher_liste_gestion_produits($liste_produits, $liste_categories, $liste_tva);
 
-	}
+    }
 
-	elseif (isset($_REQUEST["creer_categorie"]))
-	{
-		// Cas : Création d'un catégorie (affichage du formulaire)
-		Vue_formulaire_modification_categorie(true);
-	}
+    elseif (isset($_REQUEST["creer_categorie"]))
+    {
+        // Cas : Création d'un catégorie (affichage du formulaire)
+        Vue_formulaire_modification_categorie(true);
+    }
 
-	elseif (isset($_REQUEST["confirmation_creer_categorie"]))
-	{
-		// Cas : Création d'un catégorie (affichage du formulaire)
-		Categorie_Creer($connexion, $_REQUEST["nom_categorie"], $_REQUEST["status_categorie"]);
+    elseif (isset($_REQUEST["confirmation_creer_categorie"]))
+    {
+        // Cas : Création d'un catégorie (affichage du formulaire)
+        Categorie_Creer($connexion, $_REQUEST["nom_categorie"], $_REQUEST["status_categorie"]);
         $liste_categories = Categorie_select($connexion);
         Vue_afficher_liste_gestion_categories($liste_categories);
-	}
+    }
 
-	elseif (isset($_REQUEST["gerer_produits"]))
+    elseif (isset($_REQUEST["gerer_produits"]))
     {
         // Cas : On affiche la page de gestion des produits (format liste)
         $liste_produits = produit_select($connexion);
@@ -216,13 +253,13 @@ if(isset($_SESSION["idUtilisateur"]))
         Vue_afficher_liste_gestion_produits($liste_produits, $liste_categories, $liste_tva);
     }
 
-	elseif (isset($_REQUEST["gerer_categories"]))
+    elseif (isset($_REQUEST["gerer_categories"]))
     {
         $liste_categories = Categorie_select($connexion);
         Vue_afficher_liste_gestion_categories($liste_categories);
     }
 
-	elseif (isset($_REQUEST["modifier_categorie"]))
+    elseif (isset($_REQUEST["modifier_categorie"]))
     {
         $categorie = categorie_selectParID($connexion, $_REQUEST["liste_categories"][0]);
         Vue_formulaire_modification_categorie(
@@ -232,34 +269,31 @@ if(isset($_SESSION["idUtilisateur"]))
             $categorie["statusCategorie"]);
     }
 
-	elseif (isset($_REQUEST["idProduit"]))
-	{
-		// Cas : Un produit a été sélectionné.
-
-		$infos_produit = produit_selectParID($connexion, $_REQUEST["idProduit"]);
-		$liste_categories = Categorie_select($connexion);
+    elseif (isset($_REQUEST["idProduit"]))
+    {
+        // Cas : Un produit a été sélectionné.
+        $infos_produit = produit_selectParID($connexion, $_REQUEST["idProduit"]);
+        $liste_categories = Categorie_select($connexion);
         $liste_tva = TVA_select($connexion);
         Vue_formulaire_modification_produit($liste_categories, $infos_produit, false, $liste_tva);
-	}
+    }
 
-	elseif (!isset($_REQUEST["idCategorie"]) or $_REQUEST["idCategorie"] == -1)
-	{
-		// Cas : L'admin a choisi d'afficher toutes les catégories
-		//		 OU c'est sa première visite sur le site
+    elseif (!isset($_REQUEST["idCategorie"]) or $_REQUEST["idCategorie"] == -1)
+    {
+        // Cas : L'admin a choisi d'afficher toutes les catégories
+        //		 OU c'est sa première visite sur le site
 
-		$_REQUEST["idCategorie"] = -1;
-		$liste_produits = produit_select($connexion);
-		Vue_afficher_liste_produits($liste_produits);
-	}
+        $_REQUEST["idCategorie"] = -1;
+        $liste_produits = produit_select($connexion);
+        Vue_afficher_liste_produits($liste_produits);
+    }
 
-	else
-	{
-		// Cas : Une catégorie a été sélectionnée
-		$liste_produits_par_categorie = produit_selectParCategorie($connexion, $_REQUEST["idCategorie"]);
-		Vue_afficher_liste_produits($liste_produits_par_categorie);
-	}
-
-
+    else
+    {
+        // Cas : Une catégorie a été sélectionnée
+        $liste_produits_par_categorie = produit_selectParCategorie($connexion, $_REQUEST["idCategorie"]);
+        Vue_afficher_liste_produits($liste_produits_par_categorie);
+    }
 }
 
 else
